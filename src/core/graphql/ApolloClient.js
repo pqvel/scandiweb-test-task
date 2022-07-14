af186960b4ae66e795d7b4d5fccc20de6ebbec7b
Pavel Solyanik <solyqnik@gmail.com>
2022-07-14 20:41:53 +0300

    fix trouble with wrong data in product jacket

--- src/core/graphql/ApolloClient.js
+++ src/core/graphql/ApolloClient.js
@@ -1,8 +1,18 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, InMemoryCache, defaultDataIdFromObject } from '@apollo/client'
 
 const apolloClient = new ApolloClient({
   uri: 'http://localhost:4000',
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    dataIdFromObject: object => {
+      // FIXME: workaround buggy apollo cache, dont cache certain types at all!
+      switch (object.__typename) {
+        case 'AttributeSet':
+          return Math.random()
+        default:
+          return defaultDataIdFromObject(object) // fall back to default handling
+      }
+    }
+  })
 })
 
 export default apolloClient
--- src/core/redux/slices/productsSlice.js
+++ src/core/redux/slices/productsSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk, createAction } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { generateId } from '../../helpers/generateId'
 import productService from '../../services/productService'
 
